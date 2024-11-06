4e8cf71dfd50f0b6cdcdb3569ad3a41f609db7f3
Zach <62777429+ibphantom@users.noreply.github.com>
2024-11-06 18:47:19 +0800

    Create Molecule.ts

---
+++ src/components/Molecule.ts
@@ -0,0 +1,54 @@
+import * as THREE from 'three';
+import { Atom } from './Atom';
+import { Bond } from './Bond';
+
+export class Molecule extends THREE.Group {
+  public atoms: Atom[] = [];
+  public bonds: Bond[] = [];
+
+  constructor() {
+    super();
+  }
+
+  addAtom(atom: Atom) {
+    this.atoms.push(atom);
+    this.add(atom);
+  }
+
+  addBond(atom1: Atom, atom2: Atom, bondType: 'single' | 'double' | 'triple' = 'single') {
+    const bond = new Bond(atom1, atom2, bondType);
+    this.bonds.push(bond);
+    this.add(bond);
+  }
+
+  removeAtom(atom: Atom) {
+    // Remove bonds associated with the atom
+    this.bonds = this.bonds.filter(bond => {
+      if (bond.atom1 === atom || bond.atom2 === atom) {
+        this.remove(bond);
+        return false;
+      }
+      return true;
+    });
+
+    // Remove the atom
+    this.atoms = this.atoms.filter(a => a !== atom);
+    this.remove(atom);
+  }
+
+  getFormula(): string {
+    const elementCounts: { [key: string]: number } = {};
+    this.atoms.forEach(atom => {
+      const symbol = atom.elementData.symbol;
+      elementCounts[symbol] = (elementCounts[symbol] || 0) + 1;
+    });
+
+    // Generate molecular formula
+    let formula = '';
+    for (const symbol in elementCounts) {
+      formula += symbol + (elementCounts[symbol] > 1 ? elementCounts[symbol] : '');
+    }
+
+    return formula;
+  }
+}
